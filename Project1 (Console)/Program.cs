a691c3d1c68bc6ece15c33f0b4c16e7966316b26
furkan tekkol <tekkol.furkan@gmail.com>
2022-02-09 21:50:45 +0300

    Proje dosyası ekle.

---
+++ Project1 (Console)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project1__Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen adınızı girininiz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Lütfen soyadınızı girininiz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Lütfen yaşınızı:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Kişi Bilgileri:");
+            Console.WriteLine("İsim: " + name);
+            Console.WriteLine("Soyisim: " + surname);
+            Console.WriteLine("Yaş: " + age);
+            Console.ReadLine();
+        }
+    }
+}
